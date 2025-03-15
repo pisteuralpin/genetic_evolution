070a06e16d018ee06b9f3eb0e9798c3eb6000493
Pisteur_alpin <pisteur.alpin@gmail.com>
2025-03-15 19:00:33 +0100

    ➕ Add tasks system (chilling and moving)

--- src/scripts/being.js
+++ src/scripts/being.js
@@ -1,5 +1,20 @@
 import { normalDraw } from "./utility.js";
 
+const tasks = [
+    {
+        name: 'chilling',
+        weight: 8,
+        end: 'time',
+        mean_duration: 50,
+        stdev_duration: 10
+    },
+    {
+        name: 'moving',
+        weight: 2,
+        end: 'goal'
+    }
+];
+
 class Being {
     constructor(position, maxAge, maxSize, color, speed) {
         this.position = position;
@@ -11,19 +26,74 @@ class Being {
         this.growthRate = (this.maxSize - this.size) / (this.maxAge * normalDraw(0.2, 0.05));
         this.color = color;
         this.speed = speed;
+
+        this.currentTask = null;
     }
 
     update(beings) {
         this.age++;
-        console.log(this.age / this.maxAge);
         if (this.size < this.maxSize) {
             this.size += this.growthRate;
         }
         if (this.age >= this.maxAge) {
             beings.splice(beings.indexOf(this), 1);
         }
-        this.position.x += (Math.random()-.5) * this.speed;
-        this.position.y += (Math.random()-.5) * this.speed;
+
+        if (this.currentTask === null) {
+            this.decideTask();
+        }
+        else {
+            this.performTask(beings);
+        }
+        if (this.currentTask !== null && this.currentTask.end === 'time' && this.currentTask.duration <= 0) {
+            this.currentTask = null;
+        }
+    }
+
+    decideTask() {
+        const totalWeight = tasks.reduce((acc, task) => acc + task.weight, 0);
+        let random = Math.random() * totalWeight;
+        for (let i = 0; i < tasks.length; i++) {
+            random -= tasks[i].weight;
+            if (random <= 0) {
+                this.currentTask = { ...tasks[i] };
+                if (this.currentTask.end === 'time') {
+                    this.currentTask.duration = normalDraw(this.currentTask.mean_duration, this.currentTask.stdev_duration, 0);
+                }
+                else if (this.currentTask.end === 'goal') {
+                    this.currentTask.goal = null;
+                }
+                break;
+            }
+        }
+    }
+
+    performTask(beings) {
+        if (this.currentTask.name === 'chilling') {
+            if (this.currentTask.end === 'time') {
+                this.currentTask.duration--;
+            }
+        }
+        else if (this.currentTask.name === 'moving') {
+            if (this.currentTask.end === 'goal') {
+                if (this.currentTask.goal === null) {
+                    this.currentTask.goal = {
+                        x: Math.random() * (canvas.width - 100) + 50,
+                        y: Math.random() * (canvas.height - 100) + 50
+                    }
+                }
+                const dx = this.currentTask.goal.x - this.position.x;
+                const dy = this.currentTask.goal.y - this.position.y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+                if (distance > this.speed) {
+                    this.position.x += dx / distance * this.speed;
+                    this.position.y += dy / distance * this.speed;
+                }
+                else {
+                    this.currentTask = null;
+                }
+            }
+        }
     }
 
     draw(ctx) {
